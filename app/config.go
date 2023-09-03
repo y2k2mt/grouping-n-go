ca094b091e3ea897b63b5edb65df32f747405b4d
y2k2mt <y2.k2mt@gmail.com>
2023-09-03 14:33:55 +0900

    RUN_ENV parser

--- app/config.go
+++ app/config.go
@@ -1,11 +1,34 @@
 package app
 
 import (
+  "fmt"
 	"github.com/kelseyhightower/envconfig"
 )
 
+type RunEnv int
+
+const (
+  Development RunEnv = iota
+  Staging
+  Production
+)
+
+func (m *RunEnv) Decode(value string) error {
+    switch {
+    case value == "Production":
+        *m = Production
+    case value == "Staging":
+        *m = Production
+    case value == "Development":
+        *m = Development
+    default:
+        return fmt.Errorf("'%s' is an unrecognized env", value)
+    }
+    return nil
+}
+
 type Config struct {
-	Env         string `envconfig:"RUN_ENV" required:"true"`
+	Env         RunEnv `envconfig:"RUN_ENV" required:"true"`
 	DatabaseUrl string `envconfig:"DATABASE_URL" default:"host=localhost port=5432 user=postgres dbname=postgres sslmode=disable"`
 }
 
