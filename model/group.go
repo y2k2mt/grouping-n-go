3e1c72612ba03c0fcadb320bf7d5c373d6a4a2bb
y2k2mt <y2.k2mt@gmail.com>
2023-09-03 16:08:52 +0900

    add model parameter

--- model/group.go
+++ model/group.go
@@ -4,12 +4,16 @@ import (
 	"github.com/y2k2mt/grouping-n-go/infra"
 )
 
+type GroupId struct {
+	Id string
+}
+
 type GroupResult struct {
 	group *infra.Group
 }
 
-func GetGroup(id string, repository infra.GroupRepository) (GroupResult, error) {
-	group, err := repository.GetGroup(id)
+func GetGroup(id GroupId, repository infra.GroupRepository) (GroupResult, error) {
+	group, err := repository.GetGroup(id.Id)
 	if err != nil {
 		return GroupResult{}, err
 	}
