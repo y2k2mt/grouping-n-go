8d26d42764969f4b86c01e83837db74e0e6a49f2
y2k2mt <y2.k2mt@gmail.com>
2023-09-04 00:03:05 +0900

    WIP

--- model/group.go
+++ model/group.go
@@ -12,6 +12,24 @@ type GroupResult struct {
 	group *infra.Group
 }
 
+type Candidates struct {
+	n       int
+	members []string
+}
+
+type Group struct {
+	members []string
+}
+
+type Groups struct {
+	groups []Group
+}
+
+type IdentifiedGroups struct {
+	id     string
+	groups []Group
+}
+
 func GetGroup(id GroupId, repository infra.GroupRepository) (GroupResult, error) {
 	group, err := repository.GetGroup(id.Id)
 	if err != nil {
