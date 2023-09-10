a26aa5d5d7d0b9ee4c180929be03d7d8e77317c0
y2k2mt <y2.k2mt@gmail.com>
2023-09-10 15:30:44 +0900

    graceful shutdown

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/brpaz/echozap"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/y2k2mt/grouping-n-go/app"
 	"github.com/y2k2mt/grouping-n-go/infra"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -40,5 +45,21 @@ func main() {
 	e.POST("/grouping", handlers.GroupHandler.CreateGroup)
 	e.GET("/grouping/:id", handlers.GroupHandler.GetGroup)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	// Start server
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
